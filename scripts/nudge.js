c2c3104b4dfee57c1b011075071d1b22b0dcb110
Rezmason <jeremysachs@rezmason.net>
2017-09-07 07:13:13 -0700

    Recipes are now centered on the card.

--- scripts/nudge.js
+++ scripts/nudge.js
@@ -1,19 +1,27 @@
 function nudge(type) {
-    var jostle = type == "jostle";
+    var jostle = type == "JOSTLE";
     var cards = document.querySelectorAll("card,titlecard");
     for (var i = 0; i < cards.length; i++) {
-        var style = "transform:";
+        var cardStyle = "transform:";
         if (jostle) {
-            style += " rotate(" + (Math.random() * 2 - 1) * 1.5 + "deg)";
+            cardStyle += " rotate(" + rand(1.5) + "deg)";
         }
-        style += " translateY(" + i * -3 + "pt)"
-        style += ";"
-        cards[i].style = style;
+        cardStyle += " translateY(" + i * -3 + "pt)"
+        cardStyle += ";"
+        cards[i].style = cardStyle;
     }
     if (jostle) {
         var recipe = document.querySelectorAll("recipe")[0];
         if (recipe != null) {
-            recipe.style = "transform: rotate(" + (Math.random() * 2 - 1) * 2 + "deg);";
+            var recipeStyle = "transform:";
+            recipeStyle += " rotate(" + rand(1) + "deg)";
+            recipeStyle += " translate(" + rand(10) + "pt, " + rand(10) + "pt)";
+            recipeStyle += ";";
+            recipe.style = recipeStyle;
         }
     }
+}
+
+function rand(mult) {
+    return (Math.random() * 2 - 1) * mult;
 }
